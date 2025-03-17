0b6bfc154bc0e24e85d1179962ac1c9511d410c9
EnricoCB <enriv.budel@gmail.com>
2025-03-17 17:28:47 -0400

    lógica para inserir e sortear amigos

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nomeAmigo = inputAmigo.value.trim();
+    
+    if (nomeAmigo === '') {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+    amigos.push(nomeAmigo);
+    inputAmigo.value = '';
+    atualizarListaAmigos();
+
+}
+
+
+function atualizarListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        const itemLista = document.createElement('li');
+        itemLista.textContent = amigos[i];
+        listaAmigos.appendChild(itemLista);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Adicione pelo menos um amigo antes de sortear!');
+        return;
+    }
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Amigo sorteado: <strong>${amigoSorteado}`;
+    amigos = [];
+    atualizarListaAmigos();
+}
+
+document.getElementById('amigo').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        adicionarAmigo();
+    }
+});
